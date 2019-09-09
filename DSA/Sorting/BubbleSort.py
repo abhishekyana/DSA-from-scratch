ebf983641759548931612a80b0a0d79ef72fa98a
Abhishek <abhishekasdzxc@gmail.com>
2019-09-09 13:07:20 +0530

    Add files via upload

--- DSA/Sorting/BubbleSort.py
+++ DSA/Sorting/BubbleSort.py
@@ -1,4 +1,7 @@
 def bubbleSort(A):
+	"""
+	Bubble Sort Algorithm for change an unsorted Array into sorted array
+	"""
     for i,ii in enumerate(A):
         for j,jj in enumerate(A[:-i]):
             if A[j]>A[j+1]:
