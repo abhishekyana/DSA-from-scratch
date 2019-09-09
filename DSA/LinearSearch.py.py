48500c1516c8b18985de364561862e4358b11f92
Abhishek <abhishekasdzxc@gmail.com>
2019-09-09 13:07:53 +0530

    Delete LinearSearch.py.py

--- DSA/LinearSearch.py.py
+++
@@ -1,8 +0,0 @@
-def linearsearch(A, elem):
-	"""
-	Linear Search Algorithm that searches for an element in a n array with O(n) time complexity.
-	"""
-	for a in A:
-		if a==elem:
-			return True
-	return False
