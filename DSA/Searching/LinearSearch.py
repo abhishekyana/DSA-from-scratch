27b00e909bd3ddf6c5da6740dc7c4a0f8fcf163a
Abhishek <abhishekasdzxc@gmail.com>
2019-09-09 12:49:51 +0530

    Add files via upload

--- DSA/Searching/LinearSearch.py
+++ DSA/Searching/LinearSearch.py
@@ -1,6 +1,8 @@
 def linearsearch(A, elem):
 	"""
-	Linear Search Algorithm that searches for an element in a n array with O(n) time complexity
+	Linear Search Algorithm that searches for an element in an array with O(n) time complexity.
+	inputs: Array A and element e, that has to be searched
+	output: True/False
 	"""
 	for a in A:
 		if a==elem:
